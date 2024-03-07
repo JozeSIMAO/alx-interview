1656c9f5947204f435a5848a3ac19ec6616fd805
JozeSIMAO <jozesimao10@gmail.com>
2024-03-07 18:35:05 +0200

    pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,15 +4,16 @@ representing pascals triangle of n"""
 
 
 def pascal_triangle(n):
+    """Pascals Triangle"""
     result = [[1]]
 
-    if n <= 0:
+    if n < n:
         return []
-
-    for i in range(n - 1):
-        temp = [0] + result[-1] + [0]
-        row = []
-        for j in range(len(result[-1]) + 1):
-            row.append(temp[j] + temp[j + 1])
-        result.append(row)
+    else:
+        for i in range(n - 1):
+            temp = [0] + result[-1] + [0]
+            row = []
+            for j in range(len(result[-1]) + 1):
+                row.append(temp[j] + temp[j + 1])
+                result.append(row)
     return result
